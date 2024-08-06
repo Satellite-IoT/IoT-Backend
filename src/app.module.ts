e2a6e5a6deddab4e81db4a96a1ed202739462d15
brianhsieh01 <chyuanxie@gmail.com>
2024-08-06 16:35:01 +0800

    chore: configure TypeORM and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DevicesController } from './devices/devices.controller';
 import { DevicesService } from './devices/devices.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get<boolean>('DATABASE_SYNCHRONIZE'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController, DevicesController],
   providers: [AppService, DevicesService],
 })
